a1db0f4147b57325aefb6c6298b02e455eef1232
NirmalSDETAutomation <nirmalk9797@gmail.com>
2024-07-24 20:16:03 +0530

    added frames functionality

---
+++ SeleniumJavaAutomationFramework/src/test/java/selenium/java/day3/Day3_Frames.java
@@ -0,0 +1,45 @@
+package selenium.java.day3;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Day3_Frames {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		ChromeOptions cOptions = new ChromeOptions();
+		cOptions.addArguments("--incognito");
+		cOptions.addArguments("--start-maximized");
+			
+		driver.manage().window().maximize();
+		
+		driver.get("https://letcode.in/frame");
+				
+//		Actions scroll = new Actions(driver);
+		WebElement frame1 = driver.findElement(By.id("firstFr"));
+//		scroll.scrollToElement(frame1);
+		driver.switchTo().frame(frame1);
+		driver.findElement(By.name("fname")).sendKeys("Sam");
+		driver.findElement(By.name("lname")).sendKeys("Curran");
+	
+		driver.switchTo().defaultContent();
+		
+		WebElement frame2 = driver.findElement(By.name("googlefcPresent"));
+		driver.switchTo().frame(frame2);
+		
+		driver.findElement(By.name("email")).sendKeys("sam.curran@gmail.com");
+		
+		driver.close();
+	}
+
+}
